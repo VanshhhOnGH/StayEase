0784979f0b23b4731e8b8df1e1bf23b3ebc8db25
Vansh Kumar <vanshkumar1818@gmail.com>
2026-08-15 19:31:45 +0530

    Hide review delete button for other users

--- schema.js
+++ schema.js
@@ -19,7 +19,7 @@ module.exports.reviewSchema = joi.object({
   review: joi
     .object({
       rating: joi.number().required().min(1).max(5),
-      comment: joi.string().required().max(200),
+      comment: joi.string().required().max(500),
     })
     .required(),
 });
